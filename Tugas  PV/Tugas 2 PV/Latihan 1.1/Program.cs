c4a79a7c52dc1e315b1f307400a0086c11ca1d5e
Muhammad Rizky Prayoga_024 <mrpharyga2019@gmail.com>
2022-02-24 11:50:55 +0700

    TUGAS 2 PV (Code+ SS)

---
+++ Tugas  PV/Tugas 2 PV/Latihan 1.1/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Muhammad Rizky Prayoga (201401024)
+ * Date: 2/24/2022
+ * Time: 10:21 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+
+namespace BelajarCSHARP
+{
+	class programHW
+	{
+		static void Main (string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
+
+
---
+++ Tugas  PV/Tugas 2 PV/Latihan 1.2/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Muhammad Rizky Prayoga (201401024)
+ * Date: 2/24/2022
+ * Time: 10:27 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace BelajarCSHARP
+{
+class programHW
+	{
+		static void Main (string[] args)
+		{
+			Console.Write("Nama kamu siapa ? \n");
+			
+			string nama = Console.ReadLine();
+			Console.WriteLine("Nama kamu "+nama);
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
